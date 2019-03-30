2673e30268e796e9f71e9741c99a3be9bb755fe5
Izabela <48141487+izaleme@users.noreply.github.com>
2019-03-29 21:08:28 -0300

    Add files via upload

---
+++ JavaTrab.java
@@ -0,0 +1,13 @@
+public class JavaTrab {
+	public static void main(String[] args) {
+		Calculo calculosalario = new Calculo (40, 150.0, 3);
+		System.out.println("Salario Bruto: " +
+				calculosalario.calculoSalarioBruto());
+		System.out.println("Desconto INSS: " +
+				calculosalario.calculoDescontoInss());
+		System.out.println("Imposto de Renda: " +
+				calculosalario.calculoImpostoRenda());
+		System.out.println("Salario Líquido: " +
+				calculosalario.calculoSalarioLiquido());
+	}
+}
